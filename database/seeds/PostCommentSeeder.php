c9abe88b8e94bfd8a82a92e4dc84ea1e539ede2c
安河内竜二 <ryuji.yasu@gmail.com>
2019-10-02 22:38:24 +0900

    seeder途中まで

--- database/seeds/PostCommentSeeder.php
+++ database/seeds/PostCommentSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class Comment extends Seeder
+class PostCommentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,24 +13,24 @@ class Comment extends Seeder
     {
       $content = 'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。';
 
-      for( $i = 1 ; $i <= 20 ; $i++) {
-        $post = new Post;
-        $post->title = "$i 番目の投稿";
-        $post->author_id = 1;
-        $post->read_more = substr($content, 0, 120);
-        $post->content = $content;
-        $post->save();
+for( $i = 1 ; $i <= 20 ; $i++) {
+  $post = new Post;
+  $post->title = "$i 番目の投稿";
+  $post->author_id = 1;
+  $post->read_more = substr($content, 0, 120);
+  $post->content = $content;
+  $post->save();
 
-        $maxComments = mt_rand(3, 15);
-        for ($j=0; $j <= $maxComments; $j++) {
-          $comment = new Comment;
-          $comment->commenter = '名無し';
-          $comment->comment = substr($content, 0, 120);
-          $comment->email = 'xyz@gmail.com';
-          $comment->approved = 1;
-          $post->comments()->save($comment);
-          $post->increment('comment_count');
-        }
-      }
+  $maxComments = mt_rand(3, 15);
+  for ($j=0; $j <= $maxComments; $j++) {
+    $comment = new Comment;
+    $comment->commenter = '名無し';
+    $comment->comment = substr($content, 0, 120);
+    $comment->email = 'xyz@gmail.com';
+    $comment->approved = 1;
+    $post->comments()->save($comment);
+    $post->increment('comment_count');
+  }
+}
     }
 }
