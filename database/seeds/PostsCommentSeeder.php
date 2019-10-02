f392ae3c10dd2c288fd2c1c00298f792f54629c6
安河内竜二 <ryuji.yasu@gmail.com>
2019-10-02 22:13:22 +0900

    seeder完成

--- database/seeds/PostsCommentSeeder.php
+++ database/seeds/PostsCommentSeeder.php
@@ -12,26 +12,13 @@ class PostsCommentSeeder extends Seeder
     public function run()
     {
       $content = 'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。';
+      DB::table('posts')->insert([
+        'title' => 'test',
+        'content' => $content,
+        'author_id'=> 1,
+        'read_more'=> 'testです',
+        'comment_count'=>1,
+    ]);
 
-for( $i = 1 ; $i <= 20 ; $i++) {
-  $post = new Post;
-  $post->title = "$i 番目の投稿";
-  $post->author_id = 1;
-  $post->read_more = substr($content, 0, 120);
-  $post->content = $content;
-  $post->save();
-
-  $maxComments = mt_rand(3, 15);
-  for ($j=0; $j <= $maxComments; $j++) {
-    $comment = new Comment;
-    $comment->commenter = '名無し';
-    $comment->comment = substr($content, 0, 120);
-    $comment->email = 'xyz@gmail.com';
-    $comment->approved = 1;
-    $post->comments()->save($comment);
-    $post->increment('comment_count');
-  }
-}
-}
     }
 }
