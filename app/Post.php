67516d8f3e87a9cd00e5516db5d2f4b22ab2ac49
澤田樹里 <sawadajuri@sawadajurinoMacBook-Pro.local>
2019-10-02 22:08:36 +0900

    post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'content'];
+
+    public function comments(){
+    return $this->hasMany('Comment');
+  }
+
+  public function user(){
+    return $this->belongsTo('User');
+  }
+
 }
