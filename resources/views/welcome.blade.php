f6c9ef1ab1db7bd2c7a23a8bba63549a53bc9895
澤田樹里 <sawadajuri@sawadajurinoMacBook-Pro.local>
2019-10-10 19:58:51 +0900

    home色々足しました。途中です。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>Nocca Home Page</title>
+<title>nocca Form Produce Blog</title>
 <meta name="description" itemprop="description" content="説明文">
 <meta name="keywords" itemprop="keywords" content="A,B,C">
 <link href="https://fonts.googleapis.com/css?family=Kosugi+Maru&display=swap" rel="stylesheet">
@@ -17,7 +17,7 @@
 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<!--ロゴ -->
-	  <a class="navbar-brand" href=""><img src="images/logo.png" width="200px"></a>
+	  <a class="navbar-brand" href=""><img src="images/logo.png" class="img-fluid" width="200px"></a>
 			<!--画面を小さくした時 -->
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon">
@@ -26,7 +26,7 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
 	      </li>
 				<li class="nav-item active">
 	        <a class="nav-link" href="/blog/">Blog<span class="sr-only">(current)</span></a>
@@ -35,70 +35,44 @@
 	  </div>
 	</nav>
 </header>
-<div class="pull-left" style="width: 200px;">
-  <img src="images/hometop.JPG" width="450%">
-</div>
-<div　class="container" style="width: 300px;">
-	<h2>Profile</h2>
-	<img src="images/profile.png"  width="200px">
-	<h4>ジュリー</h4>
-	<p>岐阜県在住</p>
-	<p> farmersmarket＆cafe nocca というcafeで働いています。</p>
-	<p>cafeのホームページを作っているうちに、コードに興味を持つ。</p>
-	<p>プログラミング歴半年ぐらい。</p>
-</div>
-<div class="categories">
-	<div class="cooking">
-		<a href ="">料理</a>
+<div　class="container">
+	<div class="text-center">
+	  <img src="images/hometop.jpg" class="img-fluid">
 	</div>
-	<div class="nutrition">
-		<a href ="">栄養</a>
+	<div class="col-8 mx-auto">
+		<div class="categories">
+			<div class="cooking">
+				<h1>料理</h1>
+				<p>旬の食材を使ったレシピをお届けします。</p>
+			</div>
+			<div class="nutrition">
+				<h1>栄養</h1>
+				<p>旬の食べ物には栄養がたくさんあります。</p>
+			</div>
+			<div class="agriculture">
+				<h1>農業</h1>
+				<p>収穫するのは一瞬でも育てるのは様々な苦労があります。</p>
+			</div>
+		</div>
 	</div>
-	<div class="agriculture">
-		<a href ="">農業</a>
+	<div class="row">
+		<div class="d-none d-md-block col-md-4 mx-auto">
+				<h2>Profile</h2>
+				<img src="images/profile.png"  class="img-fluid" width="20%">
+				<h4>ジュリー</h4>
+				<p>岐阜県在住</p>
+				<p> farmersmarket＆cafe nocca というcafeで働いています。</p>
+				<p>cafeのホームページを作っているうちに、コードに興味を持つ。</p>
+				<p>プログラミング歴半年ぐらい。</p>
+		</div>
 	</div>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
 </div>
 
 <div class="row mb-2">
 	<div class="col-md-6">
-		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-			<div class="col p-4 d-flex flex-column position-static">
-				<strong class="d-inline-block mb-2 text-primary">World</strong>
-				<h3 class="mb-0">Featured post</h3>
-				<div class="mb-1 text-muted">Nov 12</div>
-				<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-				<a href="#" class="stretched-link">Continue reading</a>
-			</div>
-			<div class="col-auto d-none d-lg-block">
-				{% include icons/placeholder.svg width="200" height="250" background="#55595c" color="#eceeef" text="Thumbnail" %}
-			</div>
-		</div>
 	</div>
 	<div class="col-md-6">
 		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-			<div class="col p-4 d-flex flex-column position-static">
-				<strong class="d-inline-block mb-2 text-success">Design</strong>
-				<h3 class="mb-0">Post title</h3>
-				<div class="mb-1 text-muted">Nov 11</div>
-				<p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-				<a href="#" class="stretched-link">Continue reading</a>
-			</div>
-			<div class="col-auto d-none d-lg-block">
-				{% include icons/placeholder.svg width="200" height="250" background="#55595c" color="#eceeef" text="Thumbnail" %}
-			</div>
 		</div>
 	</div>
 </div>
